058090d8684aa10e9d8a2290b0d62b52a83ff92d
janthenelli <57114460+janthenelli@users.noreply.github.com>
2020-05-25 14:16:30 -0400

    created modal window styling, created generateModal function to generate modal info based on api results

--- js/app.js
+++ js/app.js
@@ -1,20 +1,19 @@
 const mainContainer = document.querySelector('.main-grid-container')
+const modalContainer = document.querySelector('.overlay')
 
 
-
-fetch('https://randomuser.me/api/?results=12&nat=au,es,de,fi,fr,nz,us')
+fetch('https://randomuser.me/api/?results=12&nat=au,es,fi,fr,nz,us')
     .then(response => response.json())
     //.then(data => console.log(data))
-    .then(data => generateCard(data))
-
-
-
+    .then(data => {
+        generateCard(data)
+        generateModal(data)
+    })    
 
 function generateCard(data) {
     for (let i=0; i<data.results.length; i++) {
         const card = document.createElement('div')
         card.classList.add('card-container')
-        mainContainer.appendChild(card)
         card.innerHTML = `
             <img class="thumbnail" src="${data.results[i].picture.large}">
             <div class="employee-info">
@@ -23,6 +22,43 @@ function generateCard(data) {
                 <p>${data.results[i].location.city}</p>
             </div>
         `
+        mainContainer.appendChild(card)
+    }
+}
+
+/* 
+Modal functionality: 
+    click on card, pop up modal with expanded information on that employee
+    have a left/right arrow for switching between employees
+        moving right grabs next siblings information to display 
+        moving left grabs last siblings information to display
+    have a close modal button/window click to close? 
+*/
+
+function generateModal(data) {
+    for (let i=0; i<data.results.length; i++) {
+        const modal = document.createElement('div')
+        modal.classList.add('modal-window')
+        modal.innerHTML = `
+            <p class="modal-close">X</p>
+            <div class="main-modal-info">
+                <img class="modal-img" src="${data.results[i].picture.large}">
+                <h3 class="name">${data.results[i].name.first} ${data.results[i].name.last}</h3>
+                <p>${data.results[i].email}</p>
+                <p>${data.results[i].location.city}</p>
+            </div>
+            <div class="additional-modal-info">
+                <p>${data.results[i].cell}</p>
+                <p>${data.results[i].location.street.number} ${data.results[i].location.street.name} ${data.results[i].location.city}, ${data.results[i].location.country}</p>
+                <p>Birthday: ${data.results[i].dob.date}</p>
+            </div>
+        `
+        modalContainer.appendChild(modal)
     }
     
 }
+
+$(".card-container").click(() => {
+    const index = $(this).index()
+    console.log(index)
+})
